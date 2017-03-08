c6fcc1cb1c9b7e120b4d7bf60657730d293cf946
lilongllong <2453085960@qq.com>
2017-03-08 10:36:40 +0800

    Add report initial commit.

---
+++ src/report/components/AppComponent.js
@@ -0,0 +1,7 @@
+import React, { Component, PropTypes } from 'react';
+
+export default class AppComponent extends Component {
+    render() {
+        return (<div className="tyu-app">布局</div>)
+    }
+}
---
+++ src/report/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import AppComponent from './components/AppComponent';
+
+render(<AppComponent />, document.getElementById('root'));
