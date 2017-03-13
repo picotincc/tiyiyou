fae78b18ceac71b1523a78ef9c7a2adf6d3a4e0d
lilongllong <2453085960@qq.com>
2017-03-13 12:46:34 +0800

    Fixed instance request data.

--- src/report/components/InstanceComponent.js
+++ src/report/components/InstanceComponent.js
@@ -13,28 +13,24 @@ export default class InstanceComponent extends Component {
   state = {
     data: null
   }
-  constructor(props) {
-    super(props);
-    this.data = [{
-      title: '爆发力',
-      desc: '爆发力是反映宝贝运动能力强弱的一项重要指标。在生活中很多运动项目需要爆发力，比如跳远，跑步，投掷等等，宝贝在玩追赶游戏时需要爆发力奔跑，走路遇到水坑时，需要爆发力奋力跃过。',
-    }, {
-      title: '动作协调',
-      desc: '动作协调是宝贝运动能力的基础，具备优秀的动作协调能力可以让宝贝更快学习如何做复杂的动作；在遇到水坑和障碍物时，动作协调能力强的宝贝可以更轻易的跨越或避开；跳绳、跑步等运动技能都需要用到动作协调能力。',
-    }]
-  }
 
   serializeData(value) {
-    this.setState({
-      data: {
-
+    const titles = ['手眼协调', '灵敏', '平衡', '手部协调', '爆发力', '动作协调', '耐力'];
+    const data = titles.reduce((prev, curr) => {
+      if (value[curr]) {
+        prev.push({
+          title: curr,
+          desc: value[curr].example
+        });
       }
-    });
+      return prev;
+    }, [])
+    this.setState({ data });
   }
 
   componentDidMount() {
-    Service.getInstance().fetchAdviceInfo(this.props.id).then(result => {
-
+    Service.getInstance().fetchCategoryInfo(this.props.id).then(result => {
+      this.serializeData(result.low_categories);
     });
   }
 
@@ -46,10 +42,10 @@ export default class InstanceComponent extends Component {
   }
 
   render() {
-    // if (!this.state.data) {
-    //   return (<div />);
-    // }
-    const contents = this.data.map(item => this.createContent(item));
+    if (!this.state.data) {
+      return (<div />);
+    }
+    const contents = this.state.data.map(item => this.createContent(item));
 
     return (<div className="tyu-instance">
       <img src='/imgs/实例装饰@3x.png' className='fixed-dece' />
